1f2a60588d259844af23c9e1dabe218cb5f53e97
Aswanthcp <cpaswanthpalayad@gmail.com>
2024-06-11 12:37:44 +0530

    modal updated

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -1,31 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import api from "../utils/api";
 import { useNavigate } from "react-router-dom";
 import "../styles/FormModal.css";
 
-const FormModal = ({ closeModal, modalData, url,getData }) => {
+const FormModal = ({ closeModal, modalData, url, getData }) => {
   const [updatedData, setUpdatedData] = useState({ ...modalData });
-  const navigate = useNavigate();
+  const [warehouses, setWarehouses] = useState([]);
   const handleInputChange = (e, key) => {
     setUpdatedData({
       ...updatedData,
       [key]: e.target.value,
     });
   };
-  const excludedKeys = ["id", "created_at", "updated_on"];
+  useEffect(() => {
+    // Fetch warehouse data when the component mounts
+    fetchWarehouses();
+  }, []);
+  const excludedKeys = [
+    "id",
+    "created_at",
+    "updated_on",
+    "category",
+    "supplier",
+    "",
+  ];
   const filteredKeys = Object.keys(modalData).filter(
     (key) => !excludedKeys.includes(key)
   );
 
+  const fetchWarehouses = () => {
+    api
+      .get("warehouse/")
+      .then((response) => {
+        setWarehouses(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching warehouse data: ", error);
+      });
+  };
+
+  const letterFormat = (value) => {
+    value =  value.split("_").join(' ');
+    return value
+  };
+
   const handleUpdate = () => {
-    // Logic to update the data
+    console.log(updatedData);
     api
       .put(`${url}${modalData.id}/`, updatedData)
       .then((res) => {
         if (res.status === 200) {
           alert("Updated successfully");
-          closeModal()
-          getData()
+          closeModal();
+          getData();
         } else {
           alert("Something went wrong");
         }
@@ -45,16 +72,38 @@ const FormModal = ({ closeModal, modalData, url,getData }) => {
         <div className="modal-body">
           {filteredKeys.map((key) => (
             <div key={key}>
-              <label htmlFor={key} className="form-label">
-                {key}
-              </label>
-              <input
-                type="text"
-                id={key}
-                value={updatedData[key]}
-                onChange={(e) => handleInputChange(e, key)}
-                className="form-input"
-              />
+              {key !== "warehouse" ? (
+                <div>
+                  <label htmlFor={key} className="form-label">
+                    {letterFormat(key)}
+                  </label>
+                  <input
+                    type="text"
+                    id={key}
+                    value={updatedData[key]}
+                    onChange={(e) => handleInputChange(e, key)}
+                    className="form-input"
+                  />
+                </div>
+              ) : (
+                <div>
+                  <label htmlFor={key} className="form-label">
+                    {key}
+                  </label>
+                  <select
+                    id="warehouse"
+                    value={updatedData.warehouse.id}
+                    onChange={(e) => handleInputChange(e, "warehouse")}
+                    className="form-select"
+                  >
+                    {warehouses.map((warehouse) => (
+                      <option key={warehouse.id} value={warehouse.id}>
+                        {warehouse.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              )}
             </div>
           ))}
         </div>
